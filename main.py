acf5daa85254278dac5f39c263e7237770f0de2e
Alec German <kimarimt@protonmail.com>
2022-06-02 19:37:16 -0400

    added a car to the game and got it to move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 from character import Character
 
 
@@ -12,20 +13,42 @@ screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen.clear()
 
 screen.tracer(False)
-turtle = Character()
+character = Character()
 screen.tracer(True)
+
+ycor = random.randint(-220, 220)
+car = turtle.Turtle()
+car.hideturtle()
+car.penup()
+car.shape('square')
+car.shapesize(stretch_wid=1, stretch_len=2)
+car.color('green')
+car.goto(-270, 0)
+car.showturtle()
+
+def drive():
+    car.forward(5)
+
+def reset():
+    ycor = random.randint(-220, 220)
+    car.hideturtle()
+    car.goto(-270, ycor)
+    car.showturtle()
 
 
 if __name__ == '__main__':
     game_is_on = True
-    screen.onkey(turtle.advance, 'w')
+    screen.onkey(character.advance, 'w')
     screen.listen()
 
     while game_is_on:
         screen.update()
+        drive()
 
-        if turtle.reaches_goal():
+        if character.reaches_goal():
             screen.tracer(False)
-            turtle.reset()
+            character.reset()
             screen.tracer(True)
-        
+
+        if car.xcor() > 270:
+            reset()
