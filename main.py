56b1cddf300ef9dbe38292a5cc1a52fb8300b050
Alec German <kimarimt@protonmail.com>
2022-06-02 14:06:21 -0400

    added the character on the screen

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print('Hello, World!')
+import turtle
+
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+START_POSITION = (0, -225)
+
+screen = turtle.getscreen()
+screen.title('Turtle Crossing')
+screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.clear()
+
+turtle = turtle.Turtle()
+turtle.hideturtle()
+turtle.penup()
+turtle.shape('turtle')
+turtle.color('green')
+turtle.goto(START_POSITION)
+turtle.setheading(90)
+turtle.showturtle()
 
 
 if __name__ == '__main__':
-    main()
+    screen.exitonclick()
